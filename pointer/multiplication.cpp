f31ff542057ccc552a9ba1e8d3a986347c7daeae
Alxey Shapilov <alexey.shapilov@airslate.com>
2024-04-12 02:26:53 +0400

    Matrix multiplication

---
+++ pointer/multiplication.cpp
@@ -0,0 +1,47 @@
+#include <cstdlib>
+#include <iostream>
+#define RANDOM_MIN 1
+#define RANDOM_MAX 10
+
+int main() {
+  int l{3}, m{4}, n{5};
+  int **matrix_a = new int *[l];              // l x m
+  int **matrix_b = new int *[m];              // m x n
+  int **matrix_multiplication = new int *[l]; // l x n
+
+  std::cout << "Matrix A:" << std::endl;
+  for (int i{0}; i < l; i++) {
+    matrix_a[i] = new int[m];
+    for (int j{0}; j < m; j++) {
+      matrix_a[i][j] = rand() % (RANDOM_MAX - RANDOM_MIN + 1) + RANDOM_MIN;
+      std::cout << matrix_a[i][j] << "\t";
+    }
+    std::cout << std::endl;
+  }
+
+  std::cout << std::endl << "Matrix B:" << std::endl;
+  for (int i{0}; i < m; i++) {
+    matrix_b[i] = new int[n];
+    for (int j{0}; j < n; j++) {
+      matrix_b[i][j] = rand() % (RANDOM_MAX - RANDOM_MIN + 1) + RANDOM_MIN;
+      std::cout << matrix_b[i][j] << "\t";
+    }
+    std::cout << std::endl;
+  }
+
+  for (int i{0}; i < l; i++) {
+    matrix_multiplication[i] = new int[n];
+  }
+
+  std::cout << std::endl << "Matrix multiplication:\n";
+  for (int i{0}; i < l; i++) {
+    for (int j{0}; j < n; j++) {
+      for (int k{0}; k < m; k++) {
+        matrix_multiplication[i][j] += matrix_a[i][k] * matrix_b[k][j];
+      }
+      std::cout << matrix_multiplication[i][j] << "\t";
+    }
+    std::cout << std::endl;
+  }
+  return 0;
+}
