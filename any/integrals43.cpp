e57aac532521f0ee015d6662a14333280a2fdfe1
Alxey Shapilov <alexey.shapilov@airslate.com>
2025-05-13 23:55:17 +0300

    Интегралы 4.2, 4.3

--- any/integrals43.cpp
+++ any/integrals43.cpp
@@ -3,6 +3,7 @@
 #include <gsl/gsl_integration.h>
 #include <gsl/gsl_linalg.h>
 #include <gsl/gsl_math.h>
+#include <iomanip>
 #include <iostream>
 #include <ostream>
 
@@ -41,16 +42,6 @@ double trapezoid(function<double(double)> f, double a, double b, int m) {
 }
 
 double simpson(function<double(double)> f, double a, double b, int m) {
-  double h = (b - a) / m;
-  double sum = f(a) + f(b);
-  for (int i = 1; i < m; i++) {
-    double x = a + i * h;
-    sum += 2 * f(a + i * h) + 4 * f(a + 0.5 * h);
-  }
-  return sum * h / 6.0;
-}
-
-double simpson1(function<double(double)> f, double a, double b, int m) {
   if (m % 2 != 0)
     m++;
   double h = (b - a) / m;
@@ -60,6 +51,11 @@ double simpson1(function<double(double)> f, double a, double b, int m) {
     sum += (i % 2 == 0) ? 2 * f(x) : 4 * f(x);
   }
   return sum * h / 3.0;
+}
+
+// Уточнение по Рунге-Ромбергу
+double runge_romberg(double I_h, double I_hl, int l, int r) {
+  return (pow(l, r) * I_hl - I_h) / (pow(l, r) - 1);
 }
 
 // Функции для вычисления интегралов
@@ -100,42 +96,92 @@ function<double(double)> select_function() {
   }
 }
 
+double *calculate_integrals(function<double(double)> f, double a, double b,
+                            int m) {
+  double *I = new double[5];
+  I[0] = left_rectangle(f, a, b, m);
+  I[1] = right_rectangle(f, a, b, m);
+  I[2] = mid_rectangle(f, a, b, m);
+  I[3] = trapezoid(f, a, b, m);
+  I[4] = simpson(f, a, b, m);
+
+  return I;
+}
+
 int main() {
-  // Ввод параметров
-  double a, b;
-  cout << "Введите концы промежутка интегрирования (a, b) через пробел: ";
-  cin >> a >> b;
-  int m;
-  cout << "Введите количество промежутков деления: ";
-  cin >> m;
+  setlocale(LC_ALL, "Russian");
 
-  function<double(double)> f = select_function();
+  do {
+    // Ввод параметров
+    double a, b;
+    cout << "Введите концы промежутка интегрирования (a, b) через пробел: ";
+    cin >> a >> b;
+    int m;
+    cout << "Введите количество промежутков деления: ";
+    cin >> m;
 
-  gsl_integration_workspace *workspace = gsl_integration_workspace_alloc(1000);
-  gsl_function F = {[](double x, void *params) -> double {
-                      auto &f =
-                          *static_cast<function<double(double)> *>(params);
+    function<double(double)> f = select_function();
 
-                      return f(x);
-                    },
-                    &f};
-  double exact, error_exact;
-  gsl_integration_qags(&F, a, b, 0, 1e-7, 1000, workspace, &exact,
-                       &error_exact);
-  gsl_integration_workspace_free(workspace);
+    gsl_integration_workspace *workspace =
+        gsl_integration_workspace_alloc(1000);
+    gsl_function F = {[](double x, void *params) -> double {
+                        auto &f =
+                            *static_cast<function<double(double)> *>(params);
 
-  double I;
+                        return f(x);
+                      },
+                      &f};
+    double exact, error_exact;
+    gsl_integration_qags(&F, a, b, 0, 1e-7, 1000, workspace, &exact,
+                         &error_exact);
+    gsl_integration_workspace_free(workspace);
 
-  std::cout << "\nТочное значение: " << exact
-            << "\nЛевый прямоугольник: " << (I = left_rectangle(f, a, b, m))
-            << " (Δ = " << fabs(I - exact) << ")"
-            << "\nПравый прямоугольник: " << (I = right_rectangle(f, a, b, m))
-            << " (Δ = " << fabs(I - exact) << ")"
-            << "\nСредний прямоугольник: " << (I = mid_rectangle(f, a, b, m))
-            << " (Δ = " << fabs(I - exact) << ")"
-            << "\nТрапеция: " << (I = trapezoid(f, a, b, m))
-            << " (Δ = " << fabs(I - exact) << ")"
-            << "\nСимпсон: " << (I = simpson1(f, a, b, m))
-            << " (Δ = " << fabs(I - exact) << ")\n";
+    double *I = calculate_integrals(f, a, b, m);
+
+    std::cout << "Точное значение: " << exact
+              << "\nЛевые прямоугольники: " << I[0]
+              << " (Δ = " << fabs(I[0] - exact) << ")"
+              << "\nПравые прямоугольники: " << I[1]
+              << " (Δ = " << fabs(I[1] - exact) << ")"
+              << "\nСредние прямоугольники: " << I[2]
+              << " (Δ = " << fabs(I[2] - exact) << ")" << "\nТрапеции: " << I[3]
+              << " (Δ = " << fabs(I[3] - exact) << ")" << "\nСимпсон: " << I[4]
+              << " (Δ = " << fabs(I[4] - exact) << ")\n\n";
+
+    int c;
+    std::cout << "1. Уточнить вычесления по Рунге-Ромбергу\n"
+              << "0. Назад\n";
+    std::cin >> c;
+
+    double l;
+    switch (c) {
+    case 1: {
+      std::cout << "Введите во сколько раз увеличить количество разбиений: ";
+      std::cin >> l;
+      double *I_hl = calculate_integrals(f, a, b, m * l);
+
+      cout << endl;
+      // Порядки точности для методов
+      vector<pair<int, string>> methods = {{1, "Левые прямоугольники"},
+                                           {1, "Правые прямоугольники"},
+                                           {2, "Средние прямоугольники"},
+                                           {2, "Трапеции"},
+                                           {4, "Симпсон"}};
+      cout << setw(25) << "СКФ" << setw(15) << "J(h)" << setw(15) << "|J-J(h)|"
+           << setw(15) << "J(h/l)" << setw(15) << "|J-J(h/l)|" << setw(15)
+           << "Jp" << setw(15) << "|j-Jp|" << endl;
+      for (int i{0}; i < methods.size(); i++) {
+        double refined = runge_romberg(I[i], I_hl[i], l, methods[i].first);
+        cout << setw(25) << methods[i].second << setw(15) << I[i] << setw(15)
+             << fabs(exact - I[i]) << setw(15) << I_hl[i] << setw(15)
+             << fabs(exact - I_hl[i]) << setw(15) << refined << setw(15)
+             << fabs(exact - refined) << endl;
+      }
+      cout << endl;
+      break;
+    }
+    }
+  } while (true);
+
   return 0;
 }
